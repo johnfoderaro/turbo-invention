f10ef38df5856700d9300454d5c6218ac2ddab00
John <johnfoderaro@icloud.com>
2018-06-27 22:10:39 -0400

    updating readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Basic React Boiler Plate
+
+- React
+- Webpack 4
+- Hot module reload
+- Babel
+- eslint
+- Production buid code splitting
+
+### MIT
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-// import { injectGlobal } from 'styled-components';
+import { injectGlobal } from 'styled-components';
 
-// injectGlobal`
-//   * {
-//     box-sizing: border-box;
-//     margin: 0;
-//     padding: 0;
-//   }
-//   @font-face {
-//     font-family: 'Source Sans Pro Regular';
-//     src: url('/sourcesanspro-regular.ttf');
-//   }
-//   body,
-//   select {
-//     font-family: 'Source Sans Pro Regular', sans-serif;
-//     font-size: ${baseFont};
-//     color: ${black};
-//   }
-// `;
+injectGlobal`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const App = () => (
   <h1>
